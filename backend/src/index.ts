a4f8a3d852b1414c6e31fd87c74cd8a2bfc0ce4b
KrishSharma007 <krishjangra65@gmail.com>
2025-01-11 00:00:45 +0530

    Frontend added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import medium from "./routes/userRouter";
 import blog from "./routes/blogrouter";
+import { cors } from "hono/cors";
 
 const app = new Hono();
-app.route('/api/v1/user', medium);
-app.route('/api/v1/blog', blog);
+app.use("*", cors());
+app.route("/api/v1/user", medium);
+app.route("/api/v1/blog", blog);
 export default app;
